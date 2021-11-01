632416b8a5174eab4498efc90fbd535e366d286a
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2021-11-01 19:56:33 -0400

    Move env out

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,13 +1,11 @@
-const { DefinePlugin } = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
     plugins: [
-        new DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development'),
-            }
-        }),
+        new Dotenv({
+            path: path.resolve(__dirname, '..', './.env.development'),
+        })
     ],
     devtool: 'eval-source-map',
 };
--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,13 +1,11 @@
-const { DefinePlugin } = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'production',
     plugins: [
-        new DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production'),
-            }
-        }),
+        new Dotenv({
+            path: path.resolve(__dirname, '..', './.env.production'),
+        })
     ],
     devtool: 'source-map',
 };
