5162b36ed15c98570cbc78b068c8d67a0075a5c3
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2021-11-01 19:53:32 -0400

    Add env

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,12 +1,10 @@
-const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
+    entry: path.resolve(__dirname, '..', './src/index.js'),
     module: {
         rules: [
             {
@@ -19,20 +17,20 @@ module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx']
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'lncRNA',
-            template: path.resolve(__dirname, './src/index.html'),
+            title: 'lncRNAfp',
+            template: path.resolve(__dirname, '..', './src/index.html'),
         }),
         new webpack.HotModuleReplacementPlugin(),
     ],
+    output: {
+        path: path.resolve(__dirname, '..', './dist'),
+        filename: 'bundle.js'
+    },
     devServer: {
-        static: path.resolve(__dirname, './dist'),
+        static: path.resolve(__dirname, '..', './dist'),
         hot: true,
     },
 };
