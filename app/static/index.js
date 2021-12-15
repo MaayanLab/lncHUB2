4b1e1400648f62d214ce34376e32bf22a5724748
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2021-12-14 23:51:56 -0500

    Add an appyter button; Tighten layout; Add the description

--- app/static/index.js
+++ app/static/index.js
@@ -1,16 +1,19 @@
 let appyters = '';
 
-function display_results(appyter) {
+function display_results(appyter, gene) {
     let appyter_card = $('#results__appyter-card'),
-        not_processed = $('#results__not-processed');
+        not_in_db = $('#results__not-in-db');
 
     appyter_card.hide();
-    not_processed.hide();
-    if (appyter !== '') {
+    not_in_db.hide();
+    if (appyter !== undefined) {
+        console.log('def')
         appyter_card.show()
         $('#results__appyter-iframe').attr('src', `gene\\${appyter.gene}`);
     } else {
-        not_processed.show();
+        console.log('undef')
+        not_in_db.text(`"${gene}" is missing from the database.`)
+        not_in_db.show();
     }
 }
 
@@ -20,7 +23,7 @@ function example(gene) {
 }
 
 function search(gene) {
-    let appyter = '';
+    let appyter = undefined;
 
     if (appyters === '') {
         fetch('static/appyters.json')
@@ -28,11 +31,11 @@ function search(gene) {
             .then(data => {
                 appyters = data;
                 appyter = data.filter(a => a['gene'] === gene)[0];
-                display_results(appyter);
+                display_results(appyter, gene);
             })
     } else {
         appyter = appyters.filter(a => a['gene'] === gene);
-        display_results(appyter);
+        display_results(appyter, gene);
     }
 
 }
